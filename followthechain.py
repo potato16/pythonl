55558c6728ecfe0825d8847fc0c6107092a5f1ee
Thao Van Han <nosaitx3@gmail.com>
2016-08-25 16:53:57 +0700

    keep calm and find more thing out

---
+++ followthechain.py
@@ -0,0 +1,17 @@
+"""Follow the chain , pythonchangllenge.com  shitty,
+What is chain, where, how, aaaaaa"""
+import urllib.request
+i =1
+uri='http://www.pythonchallenge.com/pc/def/linkedlist.php'
+nothing ='12345'
+while i<=400:
+#if True:
+	
+	fulluri=uri+"?nothing="+nothing
+	response = urllib.request.urlopen(fulluri)
+	html = response.read()
+	nothing=html[-5:].decode()
+	
+	i+=1
+	print(nothing)
+
